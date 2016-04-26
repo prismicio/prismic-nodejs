0448c8cc517ba0139ae205cac3951fc6eed71cfe
Erwan Loisant <eloisant@gmail.com>
2016-04-26 15:39:34 +0200

    Clean up and update README

--- README.md
+++ README.md
@@ -1,67 +1,80 @@
-## express-prismic
+## prismic-nodejs
 
-*Prismic.io support for ExpressJS*
+*Prismic.io support for NodeJS*
 
 [![npm version](https://badge.fury.io/js/express-prismic.svg)](http://badge.fury.io/js/express-prismic)
 
-This is a set of helpers to use Prismic.io in an [Express](http://expressjs.com/) application. If you're starting from scratch, our [NodeJS SDK](https://github.com/prismicio/nodejs-sdk) is a good base.
+This is a set of helpers to use Prismic.io in a [NodeJS](http://nodejs.org/) application. Currently it only contains a helper to create a preview route.
 
-### Configuration
+If you're starting from scratch, our [NodeJS SDK](https://github.com/prismicio/nodejs-sdk) is a good base.
 
-After including `express-prismic` in your `package.json`, simply import the prismic object:
-
-```javascript
-var prismic = require('express-prismic').Prismic;
-```
-
-The Prismic object is extended from the [Javascript Kit](https://github.com/prismicio/javascript-kit), so any attribute of the official kit, for example `Predicates`, is also available in the object exposed by express-prismic.
-
-It needs to be initialized with your configuration:
+### Installation
 
 ```javascript
-prismic.init({
-  apiEndPoint: "https://your-repo-name.prismic.io", // Mandatory
-  linkResolver: function(doc) { // Mandatory
-    return false;
-  },
-  accessToken: "xxx" // Optional
-});
+npm install prismic-nodejs --save
 ```
 
 ### Usage
 
-You can then create a Prismic context in your routes if you need to query your repository:
+```javascript
+var prismic = require('express-prismic');
+```
+
+The Prismic object is extended from the [Javascript Kit](https://github.com/prismicio/javascript-kit), so any attribute of the official kit, for example `Predicates`, is also available in the object exposed by express-prismic.
+
+It is recommended to create an `api()` method that will fetch the Api object for your repository, with the correct parameters:
+
+```javascript
+var ENDPOINT = "http://<your-repository>.prismic.io/api";
+var ACCESSTOKEN = null; // Only if your API is private
+var LINKRESOLVER = function(doc) { // Describe your reverse routing here
+  return '/' + doc.type + '/' + doc.id;
+}
+function api(req) {
+  // So we can use this information in the views
+  res.locals.ctx = {
+    endpoint: ENDPOINT,
+    linkResolver: LINKRESOLVER
+  };
+  return Prismic.api(ENDPOINT, {
+    accessToken: ACCESSTOKEN,
+    req: req
+  });
+}
+```
+
+You can then call it in your routes if you need to query your repository:
 
 ```javascript
 app.route('/').get(function(req, res) {
-  var p = prismic.withContext(req,res); // This will retrieve the API object asynchronously (using a promise)
-  p.getByUID('page', 'get-started', function (err, document) {
-    res.render('index-prismic', {
-      document: document
+  api(req).then(function (api) {
+    api.getByUID('page', 'get-started', function (err, document) {
+      res.render('index-prismic', {
+        document: document
+      });
     });
+  }).catch(function(err) {
+    // Don't forget error management
+    res.status(500).send("Error 500: " + err.message);
   });
 });
 ```
 
-Several helper functions are available to query your repository. The most flexible is the `query` function:
-
-```javascript
-  var p = prismic.withContext(req,res); // This will retrieve the API object asynchronously (using a promise)
-  p.query(prismic.Predicate.fulltext('document', term), {
-    page: 2,
-    pageSize: 10
-  }, function (err, results) {
-    [...]
-  });
-```
-
 ### Previews
 
-You can preview any document including drafts in your production site, securely. All you have to do is:
+You can preview any document including drafts in your production site, securely. All you have to do is include this route:
 
-* Include this route: `app.route('/preview').get(prismic.preview);`
+```javascript
+app.route('/preview').get(function(req, res) {
+  api(req).then(function(api) {
+    return Prismic.preview(api, configuration.linkResolver, req, res);
+  }).catch(function(err) {
+    handleError(err, req, res);
+  });
+});
+```
+
+Then:
 * Configure the URL to that preview route in the settings of your repository
 * Make sure that the [Prismic Toolbar](https://developers.prismic.io/documentation/developers-manual#prismic-toolbar) is included in your views
-
-
 
--- index.js
+++ index.js
@@ -1,153 +1,20 @@
 var Prismic = require('prismic.io');
 var Cookies = require('cookies');
 
-var configuration = {};
-
-Prismic.ErrorCodes = {
-  NOT_FOUND: 'NOT_FOUND'
-};
-
-Prismic.createError = function(status, message) {
-  var err = new Error(message);
-  err.status = status;
-  return err;
-};
-
-Prismic.init = function(config) {
-  configuration = config;
-};
-
-Prismic.getApiHome = function(accessToken) {
-  if (!configuration.apiEndpoint) {
-    return Promise.reject(new Error("Missing apiEndpoint in configuration: make sure to call init() at the beginning of your script"));
-  }
-  return Prismic.Api(configuration.apiEndpoint, accessToken).catch(function(err) {
-    switch (err.status) {
-    case 404:
-      throw new Error("Invalid apiEndPoint configuration: " + configuration.apiEndpoint);
-    default:
-      throw err;
-    }
-  });
-};
-
-function prismicWithCTX(ctxPromise, req, res) {
-  var self = {
-
-    // Return the document corresponding to the requested UID (User-readable ID)
-    'getByUID' : function(type, uid, callback) {
-      return self.queryFirst(['at','my.'+type+'.uid',uid], callback);
-    },
-    // Return a bookmark from its identifier (string)
-    'getBookmark' : function(bookmark, callback) {
-      return ctxPromise.then(function(ctx){
-        res.locals.ctx = ctx;
-        var id = ctx.api.bookmarks[bookmark];
-        if (id) {
-          self.getByID(id, callback);
-        } else {
-          callback(new Error("Error retrieving boomarked id"));
-        }
-      });
-    },
-    // Return a set of document from their ids
-    'getByIDs' : function(ids, callback) {
-      return self.query(['any', 'document.id', ids], null, callback);
-    },
-    // Return the document corresponding to the requested id
-    'getByID' : function(id, callback) {
-      return self.queryFirst(['at', 'document.id', id], callback);
-    },
-    // Return the first document matching the query
-    'queryFirst' : function(q, callback) {
-      return self.query(q, null, function(err, response) {
-        if(err) {
-          callback(err, null);
-        } else if(response && response.results && response.results[0]) {
-          callback(null, response.results[0]);
-        } else {
-          callback(Prismic.createError(404, "No document matching query"), null);
-        }
-      });
-    },
-    // Return the documents matching the query. The following options are available:
-    // page: number, the page to retrieve, starting at 1, default to 1
-    // pageSize: number, size of a page, default to 20
-    // fetch: restrict the results to some fields, separated by commas
-    // fetchLinks: include additional fields to links, separated by commas
-    'query' : function(q, options, callback) {
-      return ctxPromise.then(function(ctx) {
-        res.locals.ctx = ctx;
-        var opts = options || {};
-        if (!opts.ref) {
-          opts.ref = ctx.ref;
-        }
-        return ctx.api.query(q, opts, callback);
-      }).catch(function(err) {
-        callback(err);
-      });
-    }
-  };
-  return self;
-}
-
-Prismic.withContext = function(req, res, callback) {
-  var accessToken = (req.session && req.session['ACCESS_TOKEN']) || configuration.accessToken;
-  var ctxPromise = Prismic.getApiHome(accessToken).then(function (api) {
-    if (!configuration.linkResolver) {
-      return Promise.reject(new Error("Missing linkResolver in configuration: make sure to call init() at the beginning of your script"));
-    }
-    var cookies = new Cookies(req, res);
-    var ctx = {
-      endpoint: configuration.apiEndpoint,
-      api: api,
-      ref: cookies.get(Prismic.experimentCookie) || cookies.get(Prismic.previewCookie) || api.master(),
-      linkResolver: function(doc) {
-        return configuration.linkResolver(doc, ctx);
-      }
-    };
-    return ctx;
-  });
-  if (callback) {
-    return ctxPromise.then(function(ctx){
-      res.locals.ctx = ctx;
-      callback(null, ctx);
-    }).catch(function(err){
-      console.log(err) ;
-      callback(err, null);
+Prismic.preview = function(api, linkResolver, req, res) {
+  var token = req.query['token'];
+  if (token) {
+    api.previewSession(token, linkResolver, '/').then(function(url) {
+      var cookies = new Cookies(req, res);
+      cookies.set(Prismic.previewCookie, token, { maxAge: 30 * 60 * 1000, path: '/', httpOnly: false });
+      res.redirect(301, url);
+    }).catch(function(err) {
+      res.status(500).send("Error 500 in preview: " + err.message);
     });
   } else {
-    return prismicWithCTX(ctxPromise, req, res);
+    res.send(400, "Missing token from querystring");
   }
 };
-
-Prismic.preview = function(req, res) {
-  Prismic.withContext(req, res, function then(err, ctx) {
-    if(err) {
-      if (err.status == 404) {
-        res.status(404).send("404 not found");
-      } else {
-        res.status(500).send("Error 500: " + err.message);
-      }
-    }
-    var token = req.query['token'];
-    if (token) {
-      ctx.api.previewSession(token, ctx.linkResolver, '/', function(err, url) {
-        if (err) {
-          res.status(500).send("Error 500 in preview: " + err.message);
-        } else {
-          var cookies = new Cookies(req, res);
-          cookies.set(Prismic.previewCookie, token, { maxAge: 30 * 60 * 1000, path: '/', httpOnly: false });
-          res.redirect(301, url);
-        }
-      });
-    } else {
-      res.send(400, "Missing token from querystring");
-    }
-  });
-};
-
-Prismic.Prismic = Prismic; // Backward compatibility
 
 module.exports = Prismic;
 
