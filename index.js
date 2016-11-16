2c58963ce9eec5406292a9ec57f849f3e72acc07
Emilien Taque <eta@zengularity.com>
2016-11-16 10:20:14 +0100

    use quick route helpers extracted to javascript kit

--- index.js
+++ index.js
@@ -1,155 +1,41 @@
 var Prismic = require('prismic.io');
+var QuickRoutes = Prismic.QuickRoutes;
 var Cookies = require('cookies');
 
-const dynRoutes = [
-  {
-    "id": "bloghome",
-    "mask": "bloghome",
-    "fragments": [],
-    "fetchers": [
-      {"name": "bloghome", "mask": "bloghome", "condition": {"kind": "singleton"}}
-    ],
-    "view": "bloghome",
-  }, {
-    "id": "post",
-    "mask": "post",
-    "fragments": [
-      {"kind": "static", "value": "blog"},
-      {"kind": "dynamic", "key": "post_uid"}
-    ],
-    "fetchers": [
-      {"name": "post", "mask": "post", "condition": {"kind": "withUid", "key": "post_uid"}},
-    ],
-    "view": "post",
-  }
-]
-
-var Kind = {
-  Dynamic: 'dynamic',
-  Static: 'static'
-}
-
-var Condition = {
-  ID: 'withId',
-  UID: 'withUid',
-  Singleton: 'singleton'
-}
-
-function buildURL(fragments) {
-  if(!fragments || fragments.length == 0) return '/'
-  else {
-    return fragments.reduce(function (acc, f) {
-      switch(f.kind) {
-        case Kind.Dynamic:
-          return acc + '/:' + f.key
-
-        case Kind.Static:
-          return acc + '/' + f.value
-
-      }
-    }, '')
-  }
-}
-
-function mergeJson(obj, part) {
-  var res = Object.assign({}, obj)
-  Object.keys(part).forEach(function (key) {
-    return res[key] = part[key]
-  })
-  return res
-}
-
-function fetchData(req, res, fetchers) {
-  return new Promise((resolve, reject) => {
-    var pData = fetchers.map(function(f, index) {
-      switch(f.condition.kind) {
-      case Condition.ID:
-        return req.prismic.api.getByID(f.mask, req.params[f.condition.key])
-          .then(function(doc) {
-            var obj = {}
-            obj[f.name] = doc
-            return obj
-          })
-          .catch(function(err) {
-            reject(err)
-          })
-
-        case Condition.UID:
-          return req.prismic.api.getByUID(f.mask, req.params[f.condition.key])
-          .then(function(doc) {
-            var obj = {}
-            obj[f.name] = doc
-            return obj
-          })
-          .catch(function(err) {
-            reject(err)
-          })
-
-        case Condition.Singleton:
-          return req.prismic.api.getSingle(f.mask)
-          .then(function(doc) {
-            var obj = {}
-            obj[f.name] = doc
-            return obj
-          })
-          .catch(function(err) {
-            reject(err)
-          })
-      }
-    })
-    Promise.all(pData).then(function(results) {
-      resolve(results.reduce(function(acc, res) {
-        return mergeJson(acc, res)
-      }, {}))
-    })
-  })
-}
-
-function buildReverseRouter(dynRoutes, linkResolver) {
-  return function(doc) {
-    var docRoute = dynRoutes.find(function(dr) {
-      return dr.mask === doc.type
-    })
-    if(docRoute) return buildURL(docRoute.fragments)
-    else return linkResolver(doc)
-  }
-}
-
-Prismic.init = function(app, config, manualRouting, handleError) {
+Prismic.init = function(app, config, handleError) {
   if(!config || !config.apiEndpoint) throw 'Missing Prismic Api Endpoint';
 
+  return Prismic.api(config.apiEndpoint, config.accessToken)
+    .then(api => api.quickRoutes())
+    .then(quickRoutes => {
 
-  app.route('*').get((req, res, next) => {
-    Prismic.api(config.apiEndpoint, config.accessToken)
-    .then((api) => {
-      req.prismic = {}
-      req.prismic.api = api
-      res.locals.ctx = {
-        linkResolver: buildReverseRouter(dynRoutes, config.linkResolver),
-        endpoint: config.apiEndpoint
-      }
-      next()
-    })
-  })
+      app.route('*').get((req, res, next) => {
+        Prismic.api(config.apiEndpoint, config.accessToken)
+          .then((api) => {
+            req.prismic = { api: api };
+            res.locals.ctx = {
+              linkResolver: QuickRoutes.buildReverseRouter(quickRoutes, config.linkResolver),
+              endpoint: config.apiEndpoint
+            };
+            next();
+          })
+          .catch((err) => next(err));
+      });
 
-  dynRoutes.map(function(route, index) {
-    var url = buildURL(route.fragments)
-    app.route(url).get(function(req, res, next) {
-      fetchData(req, res, route.fetchers)
-      .then(function(data) {
-        if(manualRouting) {
-          req.prismic[route.id] = {view: route.view, data}
-          next()
-        } else {
-          res.render(route.view, data)
-        }
-      })
-      .catch(function(err) {
-        handleError(err, req, res)
-      })
-    })
-  })
-}
+      quickRoutes.filter(r => r.enabled).map(function(route, index) {
+        var url = QuickRoutes.buildURL(route.fragments);
+        app.route(url).get(function(req, res, next) {
+          QuickRoutes.fetchData(req, res, route.fetchers)
+            .then(function(data) {
+              res.render(route.view, data);
+            })
+            .catch(function(err) {
+              handleError(err, req, res);
+            });
+        });
+      });
+    });
+};
 
 Prismic.preview = function(api, linkResolver, req, res) {
   var token = req.query['token'];
@@ -167,4 +53,3 @@ Prismic.preview = function(api, linkResolver, req, res) {
 };
 
 module.exports = Prismic;
-
