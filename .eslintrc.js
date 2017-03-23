ed63860db166c54274ff0db56a32a52c7d8e7f63
Sebastien Renault <srenault.contact@gmail.com>
2017-03-23 10:13:29 +0100

    Update code following eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,6 @@
 module.exports = {
   "rules": {
-    "indent": [2, 2,
-               {"VariableDeclarator": { "var": 2, "let": 2, "const": 3}}
-              ],
+    "indent": [2, 2, { "VariableDeclarator": { "var": 2, "let": 2, "const": 3 } }],
     "linebreak-style": [2, "unix"],
     "semi": [2, "always"],
     "consistent-return": 2,
--- index.js
+++ index.js
@@ -2,73 +2,73 @@ var Prismic = require('prismic.io');
 var QuickRoutes = Prismic.QuickRoutes;
 var Cookies = require('cookies');
 
-Prismic.init = (app, config, handleError) => {
+Prismic.init = function(app, config, handleError) {
   if (!config || !config.apiEndpoint) throw 'Missing Prismic Api Endpoint';
 
   // catch all routes to attach api and context
   function setupMiddleware (app)  {
     app.route('*').get(function prismicMiddleware(req, res, next) {
-      Prismic.api(config.apiEndpoint, config.accessToken)
-        .then((api) => {
-          const pQuickRoutes = api.quickRoutesEnabled()  ? api.quickRoutes() : Promise.resolve([])
-          return pQuickRoutes.then(quickroutes => {
-            //refresh app object with new quick routes
-            hotReloadRoutes(app, quickroutes);
+      Prismic.api(config.apiEndpoint, config.accessToken).then(function(api) {
+        var pQuickRoutes = api.quickRoutesEnabled() ? api.quickRoutes() : Promise.resolve([]);
+        return pQuickRoutes.then(function(quickroutes) {
+          //refresh app object with new quick routes
+          hotReloadRoutes(app, quickroutes);
 
-            //configure prismic context
-            req.prismic = { api: api };
-            res.locals.ctx = {
-              endpoint: config.apiEndpoint,
-              linkResolver: QuickRoutes.makeLinkResolver(quickroutes, config.linkResolver)
-            };
+          //configure prismic context
+          req.prismic = { api: api };
+          res.locals.ctx = {
+            endpoint: config.apiEndpoint,
+            linkResolver: QuickRoutes.makeLinkResolver(quickroutes, config.linkResolver)
+          };
+          next();
+        });
+      }).catch(function(err) {
+        if (handleError) {
+          handleError(err, req, res);
+        } else {
+          next(err);
+        }
+      });
+    });
+  }
+
+  // use quick routes to generate router
+  function setupQuickRoutes(app, quickroutes) {
+    quickroutes.filter(function(r) {
+      return r.enabled;
+    }).map(function(route) {
+      app.route(QuickRoutes.toUrl(route)).get(function(req, res, next) {
+        QuickRoutes.fetchData(req, res, route.fetchers).then(function(data) {
+          if (!data[route.forMask]) {
+            res.status(404);
             next();
-          });
-        })
-        .catch((err) => {
+          } else {
+            res.render(route.view, data);
+          }
+        }).catch(function(err) {
           if (handleError) {
             handleError(err, req, res);
           } else {
             next(err);
           }
         });
-    });
-  };
-
-  // use quick routes to generate router
-  function setupQuickRoutes(app, quickroutes) {
-    quickroutes.filter(r => r.enabled).map((route, index) => {
-      app.route(QuickRoutes.toUrl(route)).get((req, res, next) => {
-        QuickRoutes.fetchData(req, res, route.fetchers)
-          .then((data) => {
-            if(!data[route.forMask]) {
-              res.status(404);
-              next();
-            } else {
-              res.render(route.view, data);
-            }
-          })
-          .catch((err) => {
-            if (handleError) {
-              handleError(err, req, res);
-            } else {
-              next(err);
-            }
-          });
       });
     });
   }
 
   function quickroutesURLs(quickroutes) {
-    return quickroutes.map((route) => QuickRoutes.toUrl(route));
+    return quickroutes.map(function(route) {
+      return QuickRoutes.toUrl(route);
+    });
   }
 
   function hotReloadRoutes (app, quickroutes) {
     //reset quickroutes
-    const routeStack = app._router.stack.reduce((acc, expressRoute) => {
-      if(!expressRoute.route) return acc.concat([expressRoute]);
+    var routeStack = app._router.stack.reduce(function(acc, expressRoute) {
+      if (!expressRoute.route) return acc.concat([expressRoute]);
 
-      const quickRouteIndex = quickroutesURLs(quickroutes).indexOf(expressRoute.route.path);
-      if(-1 != quickRouteIndex) {
+      var quickRouteIndex = quickroutesURLs(quickroutes).indexOf(expressRoute.route.path);
+      if (-1 != quickRouteIndex) {
         return acc;
       } else {
         return acc.concat([expressRoute]);
@@ -82,14 +82,14 @@ Prismic.init = (app, config, handleError) => {
   setupMiddleware(app);
 };
 
-Prismic.preview = (api, linkResolver, req, res) => {
+Prismic.preview = function(api, linkResolver, req, res) {
   var token = req.query['token'];
   if (token) {
-    api.previewSession(token, linkResolver, '/').then((url) => {
+    api.previewSession(token, linkResolver, '/').then(function(url) {
       var cookies = new Cookies(req, res);
       cookies.set(Prismic.previewCookie, token, { maxAge: 30 * 60 * 1000, path: '/', httpOnly: false });
       res.redirect(301, url);
-    }).catch((err) => {
+    }).catch(function(err) {
       res.status(500).send("Error 500 in preview: " + err.message);
     });
   } else {
